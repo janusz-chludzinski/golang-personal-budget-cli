37beda9f775a446f53dee216525aa5406ac6b829
Janusz Chludziński <78743486+sfc-gh-jchludzinski@users.noreply.github.com>
2021-04-16 17:22:27 +0200

    define structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,12 @@
 package module1
 
 // Budget stores budget information
-
+type Budget struct {
+	Max float32
+	Item []Item
+}
 // Item stores item information
+type Item struct {
+	Description string
+	Price float32
+}
